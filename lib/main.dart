1724cbba932f87b5ff64f96d4e29916836a70972
Chinaza <ugwuokechinazajames@gmail.com>
2022-08-03 14:54:12 +0100

    Created the sign in screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:zigo/constants/app_colors.dart';
+import 'package:zigo/screens/auth/signIn.dart';
 import 'package:zigo/screens/home/zigo_home.dart';
 import 'package:zigo/screens/splash/splash_four.dart';
 import 'package:zigo/screens/splash/splash_one.dart';
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: buildMaterialColor(Color(0xFF0A71DE)),
       ),
-      home: SplashScreenFour(),
+      home: SignInScreen(),
     );
   }
 }
---
+++ lib/screens/auth/signIn.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:zigo/constants/app_colors.dart';
+import 'package:zigo/constants/dimensions.dart';
+import 'package:zigo/widgets/zigo_logo.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.mainWhiteColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // stack for the top image and Zigo logo
+            Stack(
+              children: [
+                Positioned(
+                  child: Container(
+                    height: Dimensions.height50*8,
+                    width: double.maxFinite,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/signIn.png'),
+                        fit: BoxFit.contain,
+                      )
+                    ),
+                  ),
+                ),
+                Positioned(
+                  child: Container(
+                    margin: EdgeInsets.only(top: Dimensions.height50*4),
+                    child: ZigoLogo(zFontSize: Dimensions.height20*5.5, textFontSize: Dimensions.height20*2)
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: Dimensions.height50),
+            // Input fields section
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: Dimensions.width30),
+              child: Column(
+                children: [
+                  // for username
+                  TextField(
+                    decoration: InputDecoration(
+                      helperText: 'Username',
+                      helperStyle: GoogleFonts.montserrat(
+                        color: AppColors.mainColor,
+                        fontSize: Dimensions.font20,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                  // for password
+                  TextField(
+                    obscureText: true,
+                    obscuringCharacter: '*',
+                    decoration: InputDecoration(
+                      helperText: 'Password',
+                      helperStyle: GoogleFonts.montserrat(
+                        color: AppColors.mainColor,
+                        fontSize: Dimensions.font20,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                   // forget passsword section
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // for forgot password text
+                      InkWell(
+                        onTap: (){},
+                        child: Text(
+                          'Forgot Password?',
+                          style: GoogleFonts.montserrat(
+                            color: AppColors.mainColor,
+                            fontSize: Dimensions.font16,
+                          ),
+                        ),
+                      ),
+                      // sign in button
+                      ElevatedButton(
+                        onPressed: (){}, 
+                        child: Text('Sign in'),
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(Dimensions.width50*2, Dimensions.width20*2)
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            // Create account section
+            SizedBox(height: Dimensions.height50*2),
+            InkWell(
+              child: Text(
+              'Create Account',
+                style: GoogleFonts.montserrat(
+                  color: AppColors.mainColor,
+                  fontSize: Dimensions.font16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+           ),
+           SizedBox(height: Dimensions.height20),
+          ],
+        ),
+      ),
+    );
+  }
+}
